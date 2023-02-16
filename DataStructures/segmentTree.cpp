23a4c4ca69cc2e3c341ea0b96f170c88c26fcb3f
Sadik Al Barid <70191239+R1B3n-13@users.noreply.github.com>
2023-02-16 08:26:07 +0600

    add updateSegtree function in segment tree

--- DataStructures/segmentTree.cpp
+++ DataStructures/segmentTree.cpp
@@ -1,48 +1,69 @@
 /**
  * Author: Tard1grad3 (sabn1999@gmail.com)
- * Created: Monday, 31st October 2022 7:43:43 am (+6)
+ * Created: Thursday, 16th February 2023 8:24:18 am (+6)
 **/
 
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
 vector<int> v, seg;
 
-void initsegTree(int n)
+void initSegTree(int n)
 {
     v.assign(n, 0);
     seg.assign(4 * n, 0);
 }
 
-void build(int id, int lo, int hi)
+void buildSegTree(int id, int lo, int hi)
 {
     if(lo == hi) {seg[id] = v[lo]; return;}
     int mid = (lo + hi) / 2, lc = 2 * id + 1, rc = lc + 1;
-    build(lc, lo, mid);
-    build(rc, mid + 1, hi);
+    buildSegTree(lc, lo, mid);
+    buildSegTree(rc, mid + 1, hi);
     seg[id] = max(seg[lc], seg[rc]);
 }
 
-int query(int id, int lo, int hi, int l, int r)
+void updateSegTree(int id, int lo, int hi, int i, int x)
 {
-    if(l > hi || r < lo) return INT_MIN;
-    if(l <= lo && r >= hi) return seg[id];
+    if(i < lo || i > hi) return;
+    if(lo == hi) {seg[id] = x; return;}
     int mid = (lo + hi) / 2, lc = 2 * id + 1, rc = lc + 1;
-    return max(query(lc, lo, mid, l, r),
-               query(rc, mid + 1, hi, l, r));
+    updateSegTree(lc, lo, mid, i, x);
+    updateSegTree(rc, mid + 1, hi, i, x);
+    seg[id] = max(seg[lc], seg[rc]);
+}
+
+int querySegTree(int id, int lo, int hi, int l, int r)
+{
+    if(lo > r || hi < l) return INT_MIN;
+    if(lo >= l && hi <= r) return seg[id];
+    int mid = (lo + hi) / 2, lc = 2 * id + 1, rc = lc + 1;
+    return max(querySegTree(lc, lo, mid, l, r),
+               querySegTree(rc, mid + 1, hi, l, r));
 }
 
 int main()
 {
-    int n, q, l, r;
+    int n, q, l, r, i, x;
     cin >> n >> q;
-    initsegTree(n);
+    initSegTree(n);
     for(int i = 0; i < n; i++) cin >> v[i];
-    build(0, 0, n - 1);
+    buildSegTree(0, 0, n - 1);
     while(q--)
     {
-        cin >> l >> r;
-        l--, r--;
-        cout << query(0, 0, n - 1, l, r) << "\n";
+        cin >> i >> x >> l >> r;
+        i--, l--, r--;
+        updateSegTree(0, 0, n - 1, i, x);
+        cout << querySegTree(0, 0, n - 1, l, r) << "\n";
     }
+    return 0;
 }
+
+/*
+sample input:
+9 3
+2 3 5 1 3 9 4 6 6
+1 5 1 2
+5 10 1 6
+9 11 1 9
+*/
